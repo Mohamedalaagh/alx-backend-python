9467a736e25c57dcebb921451c020e18383c6075
Your Name <you@example.com>
2024-10-16 01:15:41 +0300

    noway

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-"""Import async_comprehension from the previous task and 
+"""Import async_comprehension from the previous task and
 generrate a measure runtime.
 """
 
 
 import asyncio
 import time
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure the total runtime"""
-    start_time = time.time()
+    '''Runs async comprehension in parallel and returns the total time'''
+    comp = __import__('1-async_comprehension').async_comprehension
 
-    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    st = time.time()
+    await asyncio.gather(comp(), comp(), comp(), comp())
 
-    end_time = time.time()
-    return end_time - start_time
+    return time.time() - st
+
+
+if __name__ == '__main__':
+    print(asyncio.run(measure_runtime()))
