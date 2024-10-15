a08d96345d12ab25b7f12790551a0f0d09d86742
Your Name <you@example.com>
2024-10-16 01:12:45 +0300

    all-done

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,10 +7,19 @@ return the 10 random numbers from random number.
 
 
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Return the 10 random numbers"""
-    results = [i async for i in async_generator()]
-    return results
+    '''returns a list of rand numbers using async comprehension'''
+
+    async_gen = __import__('0-async_generator').async_generator
+
+    rand_num = [k async for k in async_gen()]
+
+    return rand_num
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    print(asyncio.run(async_comprehension()))
